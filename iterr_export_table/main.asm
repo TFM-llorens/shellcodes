0c6bccb1dc422994a4ee200b91de4aac22a688ea
Tu Nombre <you@example.com>
2025-08-24 22:41:46 +0200

    mint

---
+++ iterr_export_table/main.asm
@@ -0,0 +1,43 @@
+[BITS 32]
+
+_start:
+    call geteip
+
+geteip:
+    pop edx
+    lea edx, [edx - 5]
+
+; buscamos en _PEB
+mov eax, fs:[0x30]              ; EAX = _PEB
+mov esi, [eax + 0x0C]           ; ESI = _PEB_LDR_DATA
+mov esi, [esi + 0x0C]           ; ESI = InLoadOrderModuleList
+mov ebx, esi                    ; guardamos el head de la lista
+
+
+lea edi, [EDX + MODULES]        ; EAX = puntero donde guardaremos direcciones
+next_module:
+    ; puntero a UNICODE_STRING.Buffer del módulo
+    mov eax, [esi + 0x2c + 0x04]      
+
+    ; guardar en nuestro array
+    mov [edi], eax
+    add edi, 4                  ; avanzar al siguiente slot
+ 
+    ; siguiente nodo en la lista
+    mov esi, [esi]              ; ESI = Flink = siguiente nodo           
+    cmp esi, ebx
+    jne next_module
+
+; -------------------------
+; almacenamiento
+MODULES:
+    m1:
+        dd 0x00000001
+    m2:
+        dd 0x00000002
+    m3:
+        dd 0x00000003
+    m4:
+        dd 0x00000004
+    m5:
+        dd 0x00000005
